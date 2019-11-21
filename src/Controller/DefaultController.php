0314aad9755491dc52d5744c92d06340e815991d
GEORGE <ellouly@hotmail.com>
2019-11-21 18:29:28 +0100

    Creation images directory

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -13,6 +13,6 @@ class DefaultController extends AbstractController
      */
     public function index() :Response
     {
-        return $this->render('wild/home.html.twig');
+        return $this->render('home.html.twig');
     }
 }
